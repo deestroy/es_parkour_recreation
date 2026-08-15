8dc0c8c7858c1c11fb548da4af7540de7ae6f32d
deestroy <dhritiaravind@gmail.com>
2026-08-14 22:39:16 -0400

    sync: protect log files from rsync --delete

--- remote/sync.sh
+++ remote/sync.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 cd "$(dirname "$0")/.."
 rsync -az --delete \
-  --exclude runs --exclude .git --exclude __pycache__ --exclude '*.pyc' \
+  --exclude runs --exclude '*.log' --exclude .git --exclude __pycache__ --exclude '*.pyc' \
   ./ claude_svpn:~/es_parkour_recreation/
 echo synced
